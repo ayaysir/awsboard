4c38f3d4823069cfc42e59cf1f3196e1bfd36c77
yoonbumtae <yoonbumtae@gmail.com>
2020-06-30 17:09:11 +0900

    Initial commit

---
+++ src/test/java/com/example/awsboard/AwsboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.awsboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
