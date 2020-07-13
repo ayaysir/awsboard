6fe415aad34db529c63c1cd819c0b3884902c604
yoonbumtae <yoonbumtae@gmail.com>
2020-07-13 19:51:24 +0900

    네이버 로그인 연동

--- src/main/java/com/example/awsboard/config/auth/OAuthAttributes.java
+++ src/main/java/com/example/awsboard/config/auth/OAuthAttributes.java
@@ -27,11 +27,19 @@ public class OAuthAttributes {
     public static OAuthAttributes of(String registrationId,
                                      String userNameAttributeName,
                                      Map<String, Object> attributes) {
+        // google, naver
+        switch (registrationId) {
+            case "naver":
+                return ofNaver("id", attributes);
+        }
+
         return ofGoogle(userNameAttributeName, attributes);
+
     }
 
     public static OAuthAttributes ofGoogle(String userNameAttributeName,
                                            Map<String, Object> attributes) {
+
         return OAuthAttributes.builder()
                 .name((String) attributes.get("name"))
                 .email((String) attributes.get("email"))
@@ -39,6 +47,21 @@ public class OAuthAttributes {
                 .attributes(attributes)
                 .nameAttributeKey(userNameAttributeName)
                 .build();
+    }
+
+    public static OAuthAttributes ofNaver(String userNameAttributeName,
+                                          Map<String, Object> attributes) {
+
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+
     }
 
     public User toEntity() {
