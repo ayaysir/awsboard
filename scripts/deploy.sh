0e595c48cc336cff4a8d86df6e0953a63e4484ab
yoonbumtae <yoonbumtae@gmail.com>
2020-07-16 16:43:42 +0900

    bug fix 6

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -25,10 +25,10 @@ JAR_NAME=$(ls -tr $REPOSITORY/ | grep jar | tail -n 1)
 echo "> JAR Name: $JAR_NAME"
 
 echo "> $JAR_NAME 에 실행권한 추가"
-chmod +x $JAR_NAME
+chmod +x $REPOSITORY/$JAR_NAME
 
 echo "> $JAR_NAME 실행"
 nohup java -jar \
         -Dspring.config.location=classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
         -Dspring.profiles.active=real \
-        $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+        $REPOSITORY/$JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
