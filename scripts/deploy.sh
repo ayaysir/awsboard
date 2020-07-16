1ee31e8d06ee7be81cb86836209ac47daaca2c86
yoonbumtae <yoonbumtae@gmail.com>
2020-07-16 16:23:13 +0900

    bug fix 4

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -29,6 +29,6 @@ chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 nohup java -jar \
-        -Dspring.config.location=classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \행
+        -Dspring.config.location=classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
         -Dspring.profiles.active=real \
         $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
