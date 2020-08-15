c82a8501dec0f1caa4335d5aedb09cb19684be0a
ayaysir <40187546+ayaysir@users.noreply.github.com>
2020-08-15 19:51:55 +0900

    Delete data.sql

--- src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-insert into midi(id, created_date, modified_date, category, custom_title,
-hash, original_file_name, original_midi_path, original_mp3path, user_id)
-values(2, sysdate(), sysdate(), 'uploaded', 'Human 1',
-'10d18a14413312513218f1b91a31751321041791f813715e', 'Allian.mid',
-'/original/Allian1.mid', '/mp3/e2cd0f42-8fb6-4f56-8e00-bba1fa05b513.mp3', 1);
