96a77bdb3973df75d4eac4e6b44f9004e162599c
yoonbumtae <yoonbumtae@gmail.com>
2020-08-16 00:16:18 +0900

    미디 재생 사파리 문제 해결

--- src/main/java/com/example/awsboard/web/MidiApiController.java
+++ src/main/java/com/example/awsboard/web/MidiApiController.java
@@ -8,8 +8,14 @@ import com.example.awsboard.web.dto.midi.MidiPublicResponseDTO;
 import com.example.awsboard.web.dto.midi.MidiRequestDTO;
 import com.example.awsboard.web.dto.midi.MidiResponseDTO;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.support.ServletContextResource;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -17,10 +23,9 @@ import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.swing.filechooser.FileSystemView;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.URLEncoder;
+import java.nio.ByteBuffer;
 import java.util.*;
 
 @RequiredArgsConstructor
@@ -135,33 +140,129 @@ public class MidiApiController {
         return result;
     }
 
-
-//    @PostMapping(DEFAULT_URI + "/t")
-//    public Map<String, Object> multi(@RequestParam("files") List<MultipartFile> files,
-//                                     @RequestParam("categories") List<String> categories,
-//                                     @RequestParam("titles") List<String> titles,
-//                                     Authentication user) {
-//
-//        Map<String, Object> result = new HashMap<>();
-//
-//        System.out.println(categories);
-//        System.out.println(titles);
-//        System.out.println(user);
-//        System.out.println(user.getAuthorities());
-//        System.out.println(user.getName());
-//        for(MultipartFile file : files) {
-//            System.out.println(file.getOriginalFilename());
-//        }
-//
-//        return result;
-//    }
-
-
     @GetMapping(DEFAULT_URI + "/mp3/{id}")
     @ResponseStatus(HttpStatus.OK)
     public void mp3Play(ModelAndView modelAndView, @PathVariable Long id,
                         HttpServletRequest request,
                         HttpServletResponse response) throws IOException {
+
+        // mp3 파일 경로 지정
+        String rootPath = FileSystemView.getFileSystemView().getHomeDirectory().toString();
+        String basePath = rootPath + "/" + "app/midi";
+        MidiResponseDTO mp3 = midiService.findById(id);
+        String mp3Path = basePath + mp3.getOriginalMp3Path();
+
+        // File 객체 생성
+        File initFile = new File(mp3Path);
+
+        // ***** RANGE 추출 ***** //
+        Long startRange = 0l;
+        Long endRange = 0l;
+        Boolean isPartialRequest = false;
+
+        try {
+            if(request.getHeader("range") != null) {
+                String rangeStr = request.getHeader("range");
+
+                System.out.println(rangeStr);
+
+                String[] range = rangeStr
+                        .replace("bytes=", "").split("-");
+
+                startRange = range[0] != null ? Long.parseLong(range[0]) : 0l;
+                if(range[1] != null) {
+                    endRange = Long.parseLong(range[1]);
+                    isPartialRequest = true;
+                }
+
+                System.out.println(">>>>> range >>>>> " + range[0] + " : " + range[1]);
+            }
+        } catch(NullPointerException | ArrayIndexOutOfBoundsException e) {
+            System.err.println(e);
+        }
+        // ****************** //
+
+        // 파일 다운로드 이름 생성
+        String downloadName = id + "-" + mp3.getCustomTitle() + ".mp3";
+        String browser = request.getHeader("User-Agent");
+
+        // 파일 인코딩
+        if(browser.contains("MSIE") || browser.contains("Trident") || browser.contains("Chrome")){
+            // 브라우저 확인 파일명 encode
+            downloadName = URLEncoder.encode(downloadName, "UTF-8").replaceAll("\\+", "%20");
+        } else {
+            downloadName = new String(downloadName.getBytes("UTF-8"), "ISO-8859-1");
+        }
+
+        // 리스폰스 헤더 설정
+        response.setHeader("Content-Disposition", "filename=\"" + downloadName +"\"");
+        response.setContentType("audio/mpeg");
+        response.setHeader("Accept-Ranges", "bytes"); // 크롬 구간문제 해결용
+        response.setHeader("Content-Transfer-Encoding", "binary;");
+
+        // 부분 범위 리퀘스트인지, 전체 범위 리퀘스트인지에 따라 Content-Range 값을 다르게
+        if(isPartialRequest) {
+            response.setHeader("Content-Range", "bytes " + startRange + "-"
+                    + endRange + "/" + initFile.length());
+        } else {
+            response.setHeader("Content-Range", "bytes 0-"
+                    + initFile.length() + "/" + initFile.length());
+            startRange = 0l;
+            endRange = initFile.length();
+        }
+
+        // 랜덤 액세스 파일을 이용해 mp3 파일을 범위로 읽기
+        try(RandomAccessFile randomAccessFile = new RandomAccessFile(initFile, "r");
+            ServletOutputStream sos = response.getOutputStream();	){
+
+            Integer bufferSize = 1024, data = 0;
+            byte[] b = new byte[bufferSize];
+            Long count = startRange;
+            Long requestSize = endRange - startRange + 1;
+
+            // startRange에서 출발
+            randomAccessFile.seek(startRange);
+
+            while(true) {
+                // 버퍼 사이즈 (1024) 보다 범위가 작으면
+                if(requestSize <= 2) {
+                    // Range byte 0-1은 아래 의미가 아님.
+                    // data = randomAccessFile.read(b, 0, requestSize.intValue());
+                    // sos.write(b, 0, data);
+
+                    // ** write 없이 바로 flush ** //
+                    sos.flush();
+                    break;
+                }
+
+                // 나머지는 일반적으로 진행
+                data = randomAccessFile.read(b, 0, b.length);
+
+                // count가 endRange 이상이면 요청 범위를 넘어선 것이므로 종료
+                if(count <= endRange) {
+                    sos.write(b, 0, data);
+                    count += bufferSize;
+                    randomAccessFile.seek(count);
+                } else {
+                    break;
+                }
+
+            }
+
+            response.setHeader("Content-Length", initFile.length() + "");
+            sos.flush();
+        }
+
+    }
+
+    @GetMapping(DEFAULT_URI + "/mp3-adv/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public ResponseEntity<Resource> mp3PlayAdv(ModelAndView modelAndView,
+                                               @PathVariable Long id,
+                                               HttpServletRequest request,
+                                               HttpServletResponse response) throws Exception {
+
         String rootPath = FileSystemView.getFileSystemView().getHomeDirectory().toString();
         String basePath = rootPath + "/" + "app/midi";
         MidiResponseDTO mp3 = midiService.findById(id);
@@ -180,26 +281,20 @@ public class MidiApiController {
             downloadName = new String(downloadName.getBytes("UTF-8"), "ISO-8859-1");
         }
 
-        response.setHeader("Content-Disposition", "filename=\"" + downloadName +"\"");
-        response.setContentType("audio/mpeg");
-        response.setHeader("Accept-Ranges", "bytes");
-        response.setHeader("Content-Length", initFile.length() + "");
-        response.setHeader("Range", "bytes=" + 0 + initFile.length());
-        response.setHeader("Content-Transfer-Encoding", "binary;");
+        InputStreamResource resource = new InputStreamResource(new FileInputStream(initFile));
 
-        try(FileInputStream fis = new FileInputStream(initFile);
-            ServletOutputStream sos = response.getOutputStream();	){
 
-            byte[] b = new byte[1024];
-            int data = 0;
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Content-Disposition", "filename=\"" + downloadName +"\"");
+        headers.set("Accept-Ranges", "bytes");
+        headers.set("Content-Length", initFile.length() + "");
+        headers.set("Content-Transfer-Encoding", "binary;");
 
-            while((data=(fis.read(b, 0, b.length))) != -1){
-                sos.write(b, 0, data);
-            }
-
-            sos.flush();
-        }
-
+        return ResponseEntity.ok()
+                .headers(headers)
+                .contentLength(initFile.length())
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .body(resource);
     }
 
     @GetMapping(DEFAULT_URI)
@@ -223,7 +318,40 @@ public class MidiApiController {
 
     public static void main(String[] args) {
         String rootPath = FileSystemView.getFileSystemView().getHomeDirectory().toString();
-        String basePath = rootPath + "/" + "app/midi/original";
+        String basePath = rootPath + "/" + "app/midi/mp3";
+
+        File file = new File(basePath + "/0ae62b33-d1c2-4969-820b-692ce027277c.mp3");
+
+        try(FileInputStream fis = new FileInputStream(file);){
+
+            byte[] b = new byte[1024];
+            int data = 0;
+            ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+
+            System.out.println("file size byte: " + file.length());
+            RandomAccessFile randomAccessFile = new RandomAccessFile(file, "r");
+            randomAccessFile.seek(102654);
+            int start = 102614, end = 673225, count = 0;
+
+            while(true) {
+                data = randomAccessFile.read(b, 0, b.length);
+                if(start <= end) {
+                    System.out.println(data);
+                    start += 1024;
+                    count += 1;
+                    randomAccessFile.seek(start);
+                } else {
+                    System.out.println(count);
+                    System.out.println(end);
+                    break;
+                }
+
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
 
     }
